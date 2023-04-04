0808809b2edac2f13d3bff9ca6a0fedbeb8b09a0
Brianna Escobar <briannaescobar6@gmail.com>
2023-04-04 16:09:41 -0400

    ADD endpoint and import tvLibrary

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors'; 
 
+import { addNewTv, getAllTv } from './src/tvLibrary';
+
 const PORT = 3030;
 
 const app = express(); 
 app.use( cors() ); 
 app.use( express.json() ); 
+
+app.get("/tvShows", getAllTv); 
+app.post("/tvShows", addNewTv); 
 
 app.listen(PORT, ()=> {
     console.log(`listening on http:localhost:${PORT}`);
--- src/tvLibrary.js
+++ src/tvLibrary.js
@@ -3,12 +3,13 @@ import{db} from "./dbConnect.js";
 export function addNewTv(req, res) {
 const newTv = req.body;
 
-db.collection('tvshow').add(newTv)
+db.collection('tvshow')
+.add (NewTv)
 .then( doc => res.status(201).send(`New Tv Added: ${doc.id}`))
 .catch( err => res.status(500).send(err))
 }
 
-export async function getAllTv(req,res){
+export async function getAllTv(req,res) {
     const collection = await db.collection('tvShows')
      .get()
     .catch(err=> res.status(500).send(err))
